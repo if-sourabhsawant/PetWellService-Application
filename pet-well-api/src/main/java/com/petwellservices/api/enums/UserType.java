bd4d9a1e300db0c6baa950599c47634affb10244
Sourabh Sawant <sourabhsawant4191@gmail.com>
2025-03-12 16:46:08 +0530

    Delete pet-well-api directory

--- pet-well-api/src/main/java/com/petwellservices/api/enums/UserType.java
+++
@@ -1,7 +0,0 @@
-package com.petwellservices.api.enums;
-
-public enum UserType {
-    VETERINARY,
-    SITTER,
-    GROOMER
-}
--- pet-well-api/src/main/java/com/petwellservices/api/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.petwellservices.api.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
